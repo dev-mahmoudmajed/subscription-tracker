f3ba50bb4961caf81b5c3f564cbdaa1e3af253a7
Eng Mahmoud Majed <mahmoudmajed404@gmail.com>
2025-06-26 18:37:57 +0300

    back-to-study

--- config/env.js
+++ config/env.js
@@ -1,6 +1,8 @@
-import dotenv from 'dotenv';
+/* eslint-disable no-undef */
+import { config } from 'dotenv';
 
-dotenv.config({ path: `.env.${process.env.NODE_ENV || 'development'}.local` });
+// es error to ts undefine its ok dont care
+config({ path: `.env.${process.env.NODE_ENV || 'development'}.local` });
 
-export const {PORT,NODE_ENV,DB_URI} = process.env;
+export const {PORT,NODE_ENV,DB_URL} = process.env;
 
--- database/mongodb.js
+++ database/mongodb.js
@@ -1,17 +1,20 @@
 import mongoose from "mongoose";
-import { DB_URI, NODE_ENV } from '../config/env.js';
+
+// eslint-disable-next-line no-unused-vars
+import { DB_URL, NODE_ENV } from '../config/env.js';
 
 
-if(!DB_URI){
+if(!DB_URL){
   throw new Error('Please define The MongoDB URI its required ');
   // process.exit(1);
 }
 const connectDB = async () => {
   try{
-    await mongoose.connect(DB_URI,{});
+    await mongoose.connect(DB_URL);
     console.log(`MongoDB connected to DB Successfully`);
   }catch (error){
     console.error(`Error: ${error.message}`);
+    // eslint-disable-next-line no-undef
     process.exit(1);
   }
 }
